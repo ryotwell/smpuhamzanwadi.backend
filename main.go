9f2b889ed0542ef3bb93571556889d35a608bbef
muhammadfaridhidayat <muhammadfaridhidayat.as4@gmail.com>
2025-10-27 16:05:36 +0800

    fix cors

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 
 	// --- CORS SETUP HERE ---
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
